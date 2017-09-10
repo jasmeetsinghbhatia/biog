7700f530525bf99716b669cd7fad8c1064fb3a46
Jasmeet Singh <jsingh@linkedin.com>
2017-09-10 01:06:00 -0700

    Added the js/css/html to the homepage

--- jasmeetsingh/views.py
+++ jasmeetsingh/views.py
@@ -10,9 +10,7 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class HomePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+    template_name = "index.html"
 
-# Add this view
 class AboutPageView(TemplateView):
     template_name = "about.html"
